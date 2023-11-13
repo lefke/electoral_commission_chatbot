cdd7305503cabb88df946b6ea624e0f60949ad17
LMacPhail <lucymacphail97@gmail.com>
2023-11-13 20:48:35 +0000

    update layout

---
+++ components/Sidebar.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export const Sidebar: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <>
+      <div className="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden">
+        <div className="flex items-center py-4">
+          <button
+            type="button"
+            className="text-gray-500 hover:text-gray-600"
+            data-hs-overlay="#application-sidebar"
+            aria-controls="application-sidebar"
+            aria-label="Toggle navigation"
+          >
+            <span className="sr-only">Toggle Navigation</span>
+            <svg
+              className="flex-shrink-0 w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div
+        id="application-sidebar"
+        className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300"
+      >
+        <div className="px-6">{children}</div>
+      </div>
+    </>
+  );
+};
