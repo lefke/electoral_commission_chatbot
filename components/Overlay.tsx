b020536b13c21b4f63adeb6ee1865fd6a5991398
lefke <ailurus@gmail.com>
2023-11-18 21:13:55 +0000

    Remove temporary modal open testing button

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -14,15 +14,6 @@ export default function LoadingModal() {
 
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-        >
-          Open dialog
-        </button>
-      </div>
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
