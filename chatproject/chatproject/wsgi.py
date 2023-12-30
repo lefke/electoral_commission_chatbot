c11eef9a5052dc08ed49899ed0391d1c36cb1e96
CardiGPT <Jethro.reeve@gmail.com>
2023-12-30 22:08:52 +0000

    weird movey stuff

--- chatproject/chatproject/wsgi.py
+++ chatproject/chatproject/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('chatproject/chatapp')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.chatproject.settings')
 
