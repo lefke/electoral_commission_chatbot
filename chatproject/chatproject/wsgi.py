8a91cb0217c98146477bb5a2a246432ec7a5df9d
CardiGPT <Jethro.reeve@gmail.com>
2023-12-30 21:59:02 +0000

    min

--- chatproject/chatproject/wsgi.py
+++ chatproject/chatproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.chatproject.settings')
 
 application = get_wsgi_application()
