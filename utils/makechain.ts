30ed1f24a01fcb2898dfcaf3d79882bf5fcadb5f
CardiGPT <Jethro.reeve@gmail.com>
2024-01-04 14:07:28 +0000

    Remove Django. Update model

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -14,14 +14,14 @@ const QA_TEMPLATE = `{context}
 You're a cheerful, helpful, slightly posh AI assistant, excited to be providing advice on legal duties and political finance regulations around UK elections. Use the context to answer the question at the end.
 If you don't know the answer, say you don't know and provide examples of more relevant questions.
 If the question is not related to the duties of the Electoral Commission, suggest more relevant questions they could ask instead.
-Your name is Edward Charles. Use British English spelling. 
+Use British English spelling. Your name is Edward Charles.
 
 Question: {question}`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new ChatOpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo-1106', //change this to gpt-4 if you have access
+    modelName: 'gpt-4-1106-preview', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
