2c93c23981958021691b94b23165447cf11ea7d4
pixelerate <lucymacphail97@gmail.com>
2024-01-29 19:41:08 +0000

    layout tweaks

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,14 +8,14 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-slate-200 text-ec-blue-900 h-screen flex flex-col">
+    <main className="bg-slate-200 text-ec-blue-900 min-h-screen flex flex-col h-full w-full">
       <Sidebar>
         <SidebarContent />
       </Sidebar>
-      <main className="w-full overflow-hidden flex sm:w-full bg-white flex-auto flex-col-reverse mt-0 p-0 justify-items-end lg:ps-64">
+      <div className="w-full overflow-hidden flex sm:w-full bg-white flex-auto flex-col-reverse mt-0 p-0 justify-items-end lg:ps-64">
         {children}
-      </main>
+      </div>
       <LoadingModal />
-    </div>
+    </main>
   );
 }
